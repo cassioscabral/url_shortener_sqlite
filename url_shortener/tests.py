d0e247f967a114afb1b0b4c68ec89a2a3e490311
Cassio Cabral <cassioscabral@gmail.com>
2013-06-05 18:50:12 -0300

    adicionando testes

--- url_shortener/tests.py
+++ url_shortener/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 from url_shortener.models import Url
 
@@ -6,15 +7,16 @@ class UrlTestCase(TestCase):
     def setUp(self):
         self.google = Url.objects.create(url_original="https://www.google.com.br/")
 
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
     def test_google_ecziste(self):
         self.assertTrue(self.google)
 
     def test_numero_clicks(self):
         self.assertEqual(self.google.numero_clicks, 0)
+
+    def test_url_modificada_nao_vazia(self):
+        self.assertIsNot(len(self.google.url_modificada), 0, "A url modificada é vazia")
+ 
+    def test_url_modificada_menor_que_original(self):
+        self.assertLess(len(self.google.url_modificada), len(self.google.url_original), "Url modificada não é menor que a original ")
+
         
