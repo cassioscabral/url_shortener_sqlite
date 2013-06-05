8c5f29ef97ae22a8e1331a67e9efecc43526f836
Cassio Cabral <cassioscabral@gmail.com>
2013-06-05 18:11:42 -0300

    testes básicos rodando

--- url_shortener/tests.py
+++ url_shortener/tests.py
@@ -1,16 +1,20 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from url_shortener.models import Url
 
+class UrlTestCase(TestCase):
 
-class SimpleTest(TestCase):
+    def setUp(self):
+        self.google = Url.objects.create(url_original="https://www.google.com.br/")
+
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+    def test_google_ecziste(self):
+        self.assertTrue(self.google)
+
+    def test_numero_clicks(self):
+        self.assertEqual(self.google.numero_clicks, 0)
+        
