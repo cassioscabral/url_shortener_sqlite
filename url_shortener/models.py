14c6826e85bc9b9831eae2a35b96e268a8b478ad
Cassio Cabral <cassioscabral@gmail.com>
2013-06-10 11:14:44 -0300

    modificando a URL no model ao salvar

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import random
+import string
 
 class Url(models.Model):
     url_original = models.URLField(max_length=255)
-    url_modificada = models.CharField(max_length=20)
+    url_modificada = models.CharField(max_length=20, blank=True)
     numero_clicks = models.IntegerField(default=0)
     
     def get_absolute_url(self):
@@ -10,15 +12,18 @@ class Url(models.Model):
 
 
     def __unicode__(self):
-        return self.url_original
+        return self.url_modificada
 
-    def palavra_aleatoria():
+    def palavra_aleatoria(self, tamanho=6):
         palavra = ""
-        for i in range(6):
+        for i in range(tamanho):
             palavra += random.choice(string.letters)
 
         return  palavra
 
+    def modifica_url(self, padrao_inicial="soda/"):
+        return padrao_inicial + self.palavra_aleatoria()
+
     def save(self, *args, **kwargs):
-        self.url_modificada = "modificada"
+        self.url_modificada = self.modifica_url()
         super(Url, self).save(*args, **kwargs)
